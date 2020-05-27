4b001520683580a173ec3e338e339f630cbc308f
damian8300 <dbouzo77@gmail.com>
2020-05-27 04:59:20 -0300

    route fix

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
 Route::post('/login', 'AuthController@login');
 
 Route::middleware('auth:api')->group(function(){
-
 
 /*
     Login
